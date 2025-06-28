650cb6b1f025dc3e157c3d631388f47211e755d4
KayGV25 <khuongvudang25@gmail.com>
2025-06-28 20:59:30 +0700

    (FIX)[KAN-21] Fix server crash on start

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import app from './app';
 import config from './configs/serverConfig';
 
+const isDev = config.nodeEnv === 'development';
+
 app.listen(config.port, () => {
-    console.log(`Server is running on port ${config.port}`);
+     const message = isDev
+        ? `Server is running at http://localhost:${config.port}`
+        : `Server is running on port ${config.port}`;
+    console.log(message);
 });
